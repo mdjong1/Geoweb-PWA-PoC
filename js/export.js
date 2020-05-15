6931d40de480df3a422ce0c987be6cb3f58ac39b
Maarten de Jong <maarten.de.jong@hotmail.com>
2020-05-15 10:01:18 +0200

    Add KML export functionality

--- js/export.js
+++ js/export.js
@@ -1,6 +1,7 @@
 $("#geojson-export").on('click', exportTrackToGeoJson);
+$("#kml-export").on('click', exportTrackToKml);
 
-let geoJsonLineString = {
+const geoJsonLineString = {
     "type": "FeatureCollection",
     "features": [
         {
@@ -14,16 +15,31 @@ let geoJsonLineString = {
     ]
 }
 
-function exportTrackToGeoJson() {
+function getTrackAsGeoJson() {
     let exportJson = copy(geoJsonLineString);
-
     exportJson["features"][0]["geometry"]["coordinates"] = flip(gpsLocations);
 
-    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportJson));
+    return exportJson;
+}
+
+function exportTrackToGeoJson() {
+    // https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
+
+    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(getTrackAsGeoJson()));
     let dlAnchorElem = document.getElementById('downloadAnchorElem');
 
     dlAnchorElem.setAttribute("href", dataStr);
     dlAnchorElem.setAttribute("download", Date.now() + ".json");
     dlAnchorElem.click();
+}
 
+function exportTrackToKml() {
+    let dataStr = "data:application/xml;charset=utf-8," + encodeURIComponent(tokml(getTrackAsGeoJson()));
+    let dlAnchorElem = document.getElementById('downloadAnchorElem');
+
+    console.log(dataStr);
+
+    dlAnchorElem.setAttribute("href", "data:" + dataStr);
+    dlAnchorElem.setAttribute("download", Date.now() + ".xml");
+    dlAnchorElem.click();
 }
